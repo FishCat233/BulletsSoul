b6e426165d9399ecff8b24d90cf6dde023d25894
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2026-02-21 15:15:23 +0800

    feat(player): 添加血条字段

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -15,6 +15,9 @@ namespace BulletsSoul.Player
 
     public class PlayerController : MonoBehaviour
     {
+        [Header("Health")]
+        [SerializeField] private float maxHealth = 100f;
+
         [Header("Movement")]
         [SerializeField] private float normalMoveSpeed = 2f;
         [SerializeField] private float sprintMoveSpeed = 2.5f;
@@ -33,6 +36,8 @@ namespace BulletsSoul.Player
         public ReactiveProperty<float> CurrentStamina { get; private set; }
         public ReactiveProperty<float> MaxStamina { get; private set; }
 
+        private float _currentHealth = 1f;
+
         private float _currentMoveSpeed;
         private float _currentStamina;
 
@@ -48,6 +53,7 @@ namespace BulletsSoul.Player
 
         private void Awake()
         {
+            _currentHealth = maxHealth;
             _currentStamina = maxStamina;
             CurrentStamina = new ReactiveProperty<float>(_currentStamina);
             MaxStamina = new ReactiveProperty<float>(maxStamina);
