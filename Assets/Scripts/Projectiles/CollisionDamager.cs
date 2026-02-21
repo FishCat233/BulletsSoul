6a925d8f2a1849ab1ca2ccd435bda29b8e1ce110
FishCat233 <50800434+FishCat233@users.noreply.github.com>
2026-02-21 21:47:17 +0800

    refactor(弹幕): 重构 bullet 组件

---
+++ Assets/Scripts/Projectiles/CollisionDamager.cs
@@ -0,0 +1,47 @@
+﻿using BulletsSoul.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BulletsSoul.Projectile
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class CollisionDamager : MonoBehaviour
+    {
+        public float damage = 10f;
+
+        private PlayerController _playerController;
+        private bool _playerInTrigger;
+
+        private void FixedUpdate()
+        {
+            if (_playerInTrigger && _playerController != null)
+            {
+                _playerController.TakeDamage(damage);
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                _playerController = collision.GetComponent<PlayerController>();
+                if (_playerController != null)
+                {
+                    _playerController.TakeDamage(damage);
+                    _playerInTrigger = true;
+                }
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                _playerInTrigger = false;
+                _playerController = null;
+            }
+        }
+    }
+}
+
